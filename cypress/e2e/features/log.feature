938886bc0bb7980e0da19e15f762b36670af2351
Kevin D Lopez <kevin.lopez@quind.io>
2024-12-18 16:16:33 -0500

    Agregue los tags

--- cypress/e2e/features/log.feature
+++ cypress/e2e/features/log.feature
@@ -1,15 +1,21 @@
-Feature: Login Functionality
-  As a user of SauceDemo
-  I want to test the login functionality
-  To ensure it works as expected
+@Login
+Feature: Inicio de sesion en Swalabs
+    Como usuario de SwagLabs
+    Quiero poder inciar sesion en la plataforma
+    Para poder ver los productos y realizar compras
   Background: 
-    Given Que el usuario esta en la pagina de incio de sesion
-  Scenario: Login successfully with valid credentials
-      When el usuario ingresa las credenciales validas 
-      Then es redireccionado a la pagina de inicio 
-  Scenario: Login successfully with wrong credentials
-      When el usuario ingresa las credenciales incorrectas
-      Then el usuario ve el mensaje de error 
-  
+    Given Que el usuario se encuentra en la pagina de incio de sesion
+  @login-success
+  Scenario: Inicio de sesíon exitoso con credenciales validas 
+      When el usuario ingresa Usuario y Password validos 
+      Then el sistema muestra la pagina principal con los productos
+ @login-error  
+  Scenario: Inicio de sesíon con usuario o password incorrectos
+      When el usuario ingresa  Usuario o Password incorrectos
+      Then el sistema muestra un mensaje de error que indica credenciales incorrectas
+   @login-locked
+  Scenario: Inicio de sesíon con usuario bloqueado 
+      When el usuario ingresa con un usuario que esta bloqueado
+      Then el sistema muestra un mensaje que indica que el usuario fue bloqueado 
 
       
--- cypress/support/step_definitions/loginPageSteps.js
+++ cypress/support/step_definitions/loginPageSteps.js
@@ -3,28 +3,38 @@ import log from "../../e2e/pageObject/Log";
 
 const users = require("../../fixtures/users.json");
 
-Given("Que el usuario esta en la pagina de incio de sesion", () => {
+Given("Que el usuario se encuentra en la pagina de incio de sesion", () => {
   log.visit("/");
 });
 
-When("el usuario ingresa las credenciales validas", () => {
+When("el usuario ingresa Usuario y Password validos", () => {
   const { username, password } = users.validUser;
   log.enterUsername(username);
   log.enterPassword(password);
   log.clickLogin();
 });
 
-When("el usuario ingresa las credenciales incorrectas", () => {
+When("el usuario ingresa  Usuario o Password incorrectos", () => {
   const { username, password } = users.invalidUser;
   log.enterUsername(username);
   log.enterPassword(password);
   log.clickLogin();
 });
+When("el usuario ingresa con un usuario que esta bloqueado",()=>{
+  const {username, password}= users.lockedUser;
+  log.enterUsername(username);
+  log.enterPassword(password);
+  log.clickLogin();
+})
 
-Then("es redireccionado a la pagina de inicio", () => {
+Then("el sistema muestra la pagina principal con los productos", () => {
   cy.url().should("include", "/inventory.html"); 
 });
 
-Then("el usuario ve el mensaje de error", () => {
+Then("el sistema muestra un mensaje de error que indica credenciales incorrectas", () => {
   cy.get("h3").should("be.visible").and("have.text", "Epic sadface: Username and password do not match any user in this service");
+});
+
+Then("el sistema muestra un mensaje que indica que el usuario fue bloqueado",()=>{
+  cy.get("h3").should("be.visible").and("have.text", "Epic sadface: Sorry, this user has been locked out.");
 });
